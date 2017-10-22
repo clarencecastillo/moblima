a05bcab5f6714512f566274cff565e23a91a43cc
Clarence Castillo <clarencecastillo@outlook.com>
2017-10-22 21:40:11 +0800

    Add missing movie status to MovieView

--- src/view/MovieView.java
+++ src/view/MovieView.java
@@ -13,16 +13,17 @@ public class MovieView extends View {
 
         setTitle(String.format("%s [%s] %s", movie.getTitle(),
                                movie.getType(), movie.getRating()));
-        setContent("Director: " + movie.getDirector(),
-                   "Actors: " + String.join(",",
-                                            Arrays.stream(movie.getActors())
-                                                  .map(String::valueOf).toArray(String[]::new)),
-                   "Runtime: " + movie.getRuntimeMinutes() + " minutes",
-                   "Score: " + (movie.getOverallReviewRating() == -1 ? "NA" :
-                                String.format("%.1f/5.0", movie.getOverallReviewRating())),
-                   " ",
-                   "Sypnosis",
-                   "--------",
-                   movie.getSypnosis());
+        setContent("Status: " + movie.getStatus().toString(),
+                "Director: " + movie.getDirector(),
+                "Actors: " + String.join(",",
+                        Arrays.stream(movie.getActors())
+                                .map(String::valueOf).toArray(String[]::new)),
+                "Runtime: " + movie.getRuntimeMinutes() + " minutes",
+                "Score: " + (movie.getOverallReviewRating() == -1 ? "NA" :
+                        String.format("%.1f/5.0", movie.getOverallReviewRating())),
+                " ",
+                "Sypnosis",
+                "--------",
+                movie.getSypnosis());
     }
 }
